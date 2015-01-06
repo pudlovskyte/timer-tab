89754881f4950497d86c5a9c9768d3104c77e6d7
Monika Pudlovskyte <monika@pudlovskyte.lt>
2015-01-06 16:26:11 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var child_process = require('child_process');
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+
+var notify = function(message){
+    if (process.platform === 'darwin') {
+        child_process.spawn(
+            'osascript',
+            ['-e', 'display notification "'+message+'" with title "LESS compilation failed"']
+        );
+    }
+};
+
+var build = function(){
+    gulp.src('src/timertab.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .on('error', function(e){
+            console.log(e.message);
+
+            // Remove long file path
+            notify(e.message.replace(/(in file) .+?\/([^\/]+\.less line no)/, '$1 $2'));
+
+            this.emit('end');
+        })
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build'));
+};
+
+gulp.task('build', build);
+
+gulp.task('default', function() {
+    build();
+    gulp.watch(['src/timertab.less', 'src/styles/*.less'], ['build']);
+});
+
