3eeca2074bc749fa24bf4c3c6e5010e37b106340
Monika Pudlovskyte <monika@pudlovskyte.lt>
2015-02-09 19:38:46 +0200

    Alarmclock js added

--- src/timertab.js
+++ src/timertab.js
@@ -12,14 +12,51 @@ $('[data-target-mode]').on('click', function(){
 	);
 });
 
-var getTime = function($form){
-	var hours = Number($form.find('.hours input').val());
-	var minutes = Number($form.find('.minutes input').val());
-	var seconds = Number($form.find('.seconds input').val());
-	return 3600*hours + 60*minutes + seconds;
+var getUserInput = function($form){
+	return {
+		hours: Number($form.find('.hours input').val()),
+		minutes: Number($form.find('.minutes input').val()),
+		seconds: Number($form.find('.seconds input').val())
+	};
 };
 
 $('form.countdown').on('submit', function(event){
 	event.preventDefault();
-	countdown(getTime($(event.target)));
+	var t = getUserInput($(event.target));
+	countdown(3600*t.hours + 60*t.minutes + t.seconds);
+});
+
+var getUserDate = function(now, $form){
+	var input = getUserInput($form);
+
+	var target = new Date(
+		now.getFullYear(),
+		now.getMonth(),
+		now.getDate(),
+		input.hours,
+		input.minutes,
+		input.seconds
+	);
+
+	if (target < now) {
+		return new Date(
+			now.getFullYear(),
+			now.getMonth(),
+			now.getDate() + 1,
+			input.hours,
+			input.minutes,
+			input.seconds
+		);
+	}
+
+	return target;
+};
+
+$('form.alarmclock').on('submit', function(event){
+	event.preventDefault();
+	var now = new Date();
+	var target = getUserDate(now, $(event.target));
+	countdown(Math.round((target - now) / 1000));
+
+	$('.alarm-time').text(target.toLocaleTimeString(undefined, {hour: '2-digit', minute:'2-digit'}));
 });
