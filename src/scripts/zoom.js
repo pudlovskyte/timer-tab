a8cad71c336140f2ca7a537e0d68d0259305ab0d
Monika Pudlovskyte <monika@pudlovskyte.lt>
2015-03-02 20:05:42 +0200

    Zoom: now with translateY for vertical centering

--- src/scripts/zoom.js
+++ src/scripts/zoom.js
@@ -2,21 +2,29 @@
 
 var calczoom = function($body, $target, window){
 	var scale = window.innerWidth / $target.width();
-	var targetPosition = window.innerWidth / 2 - ($target.width() / 2);
-	var currentPosition = $target.offset().left;
-	var translateX = targetPosition - currentPosition;
+	var targetX = window.innerWidth / 2 - ($target.width() / 2);
+	var targetY = window.innerHeight / 2 - ($target.height() / 2);
+	var {top, left} = $target.offset();
+	var translateX = targetX - left;
+	var translateY = targetY - top;
 	return {
 		scale: scale,
-		translateX: translateX
+		translateX: translateX.toString() + 'px',
+		translateY: translateY.toString() + 'px'
 	};
 };
 
 module.exports = function(window, $body){
 
 	var zoom = function($target){
-		var data = calczoom($body, $target, window);
+		var transforms = calczoom($body, $target, window);
 		$body.css('transform',
-			'scale(' + data.scale + ') translateX(' + data.translateX + 'px)');
+			Object.keys(transforms)
+				.map(function(key){
+					return key + '(' + transforms[key] + ')';
+				})
+				.join(' ')
+		);
 	};
 
 	var resetzoom = function(){
